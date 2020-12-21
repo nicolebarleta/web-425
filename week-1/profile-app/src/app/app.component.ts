9a4b000d6712cbad264bba54459ccfd7021120d7
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-12-20 18:44:31 -0600

    Adding Assignment 1.4

---
+++ week-1/profile-app/src/app/app.component.ts
@@ -0,0 +1,19 @@
+/**
+ * Title: app.component.ts
+ * Author: Professor Krasso
+ * Date: 20 December 2020
+ * Modified By: Marie Nicole Barleta
+ * Description: App component
+ */
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment: string = 'Assignment 1.5 - Components';
+}
